eb1fe1d46354c626388e4b47421930c35a6ca48f
HadiImran2022 <abdulhadiimran2022@gmail.com>
2022-11-28 17:32:00 +0500

    V

---
+++ index.js
@@ -0,0 +1,100 @@
+var textInput = document.getElementById("inp");
+var errorPera = document.getElementById("error");
+var empty = [];
+function addValue() {
+  var b = textInput.value;
+  if (b == "") {
+    error.innerHTML = "Enter a Message";
+  } else {
+    empty.push(b);
+    console.log(empty);
+    error.value = "";
+    textInput.value = "";
+  }
+}
+
+var textInput = document.getElementById("myInput");
+var errorPera = document.getElementById("myError");
+var empty = [];
+function addValue() {
+  var b = textInput.value;
+  if (b == "") {
+    error.innerHTML = "Enter a Message";
+  } else {
+    empty.push(b);
+    console.log(empty);
+    error.value = "";
+    textInput.value = "";
+  }
+}
+
+var textInput = document.getElementById("myInput2");
+var errorPera = document.getElementById("myError2");
+var empty = [];
+function addValue() {
+  var b = textInput.value;
+  if (b == "") {
+    error.innerHTML = "Enter a Message";
+  } else {
+    empty.push(b);
+    console.log(empty);
+    error.value = "";
+    textInput.value = "";
+  }
+}
+var textInput = document.getElementById("myInput2");
+var errorPera = document.getElementById("myError2");
+var empty = [];
+function addValue() {
+  var b = textInput.value;
+  if (b == "") {
+    error.innerHTML = "Enter a Message";
+  } else {
+    empty.push(b);
+    console.log(empty);
+    error.value = "";
+    textInput.value = "";
+  }
+}
+var textInput = document.getElementById("myInput3");
+var errorPera = document.getElementById("myError3");
+var empty = [];
+function addValue() {
+  var b = textInput.value;
+  if (b == "") {
+    error.innerHTML = "Enter a Message";
+  } else {
+    empty.push(b);
+    console.log(empty);
+    error.value = "";
+    textInput.value = "";
+  }
+}
+var textInput = document.getElementById("myInput4");
+var errorPera = document.getElementById("myError4");
+var empty = [];
+function addValue() {
+  var b = textInput.value;
+  if (b == "") {
+    error.innerHTML = "Enter a Message";
+  } else {
+    empty.push(b);
+    console.log(empty);
+    error.value = "";
+    textInput.value = "";
+  }
+}
+var textInput = document.getElementById("myInput5");
+var errorPera = document.getElementById("myError5");
+var empty = [];
+function addValue() {
+  var b = textInput.value;
+  if (b == "") {
+    error.innerHTML = "Enter a Message";
+  } else {
+    empty.push(b);
+    console.log(empty);
+    error.value = "";
+    textInput.value = "";
+  }
+}
